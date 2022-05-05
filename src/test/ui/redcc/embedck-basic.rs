ede79fd09064bc2e077d38e690ab4c98020e5654
Arthur Lafrance <lafrancearthur@gmail.com>
2022-05-05 01:40:42 -0700

    add full suite of embedck tests

--- src/test/ui/redcc/embedck-basic.rs
+++ src/test/ui/redcc/embedck-basic.rs
@@ -4,22 +4,24 @@
 
 #[cfg_attr(not(test), rustc_diagnostic_item = "RRef")]
 struct RRef<T>(T);
-struct ContainsRRefDirect(RRef<i32>);
+struct Direct(RRef<i32>);
+struct TwoRRefs(RRef<i32>, RRef<i32>);
 
 // Detect trivial embedding and non-embedding assignments
 fn main() {
     let r = RRef(4);
-    let mut rc = RRef(ContainsRRefDirect(RRef(1)));
+    let mut rc = RRef(Direct(RRef(1)));
     rc.0.0 = r;
 
     let r = RRef(4);
-    let mut c = ContainsRRefDirect(RRef(1));
+    let mut c = Direct(RRef(1));
     c.0 = r;
+
+    let mut r = TwoRRefs(RRef(1), RRef(2));
+    r.1 = RRef(3);
 }
 
 // more test cases:
-    // Indirect embedding
-    // Sibling RRef does not trigger embed
     // embedding with different kinds of rvalues
         // enums too
     // embedding with different kinds of places
---
+++ src/test/ui/redcc/embedck-diverse-rvals.rs
@@ -0,0 +1,37 @@
+// build-pass
+
+#![feature(rustc_attrs)]
+
+#[cfg_attr(not(test), rustc_diagnostic_item = "RRef")]
+struct RRef<T>(T);
+struct Direct(RRef<i32>);
+struct Ref<'a>(&'a RRef<i32>);
+
+enum MaybeRRef {
+    Yes(RRef<i32>),
+    No,
+}
+
+fn main() {
+    // array
+    let mut array = RRef([RRef(0), RRef(1)]);
+    array.0 = [RRef(1), RRef(2)];
+
+    // tuple
+    let mut tuple = RRef((RRef(0), RRef(1)));
+    tuple.0 = (RRef(0), RRef(1));
+
+    // reference
+    let tmp = &RRef(1);
+    let mut r = RRef(Ref(tmp));
+    r.0.0 = &RRef(0);
+
+    // enum
+    let mut m = RRef(MaybeRRef::No);
+    m.0 = MaybeRRef::Yes(RRef(1));
+
+    // raw pointer
+    let p: *const RRef<_> = &RRef(1);
+    let mut r = RRef(p);
+    r.0 = &RRef(2);
+}
